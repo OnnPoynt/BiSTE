c1559dddfac632d4bac6fb4789a5d6cba8304aba
Charan P <86849179+OnnPoynt@users.noreply.github.com>
2022-12-02 17:17:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "charansupriya@gmail.com",
+        Password: "fqbqzobkhycpyfhv",
+        To: 'reddycharan173@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "New contact form entry",
+        Body: 
+        "Name:" + document.getElementById("name").value + 
+        "<br> Company:" + document.getElementById("company").value + 
+        "<br> Email:" + document.getElementById("email").value + 
+        "<br> Phone no:" + document.getElementById("phone").value + 
+        "<br> Message:" + document.getElementById("message").value
+    }).then(
+        message => alert("message sent successfully")
+    );
+
+}
+
