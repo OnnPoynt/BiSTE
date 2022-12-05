a4455c662f7926a3426421cacf6ba71aedb45c22
Charan P <86849179+OnnPoynt@users.noreply.github.com>
2022-12-05 12:08:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "charansupriya@gmail.com",
-        Password: "fqbqzobkhycpyfhv",
-        To: 'reddycharan173@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: "New contact form entry",
-        Body: 
-        "Name:" + document.getElementById("name").value + 
-        "<br> Company:" + document.getElementById("company").value + 
-        "<br> Email:" + document.getElementById("email").value + 
-        "<br> Phone no:" + document.getElementById("phone").value + 
-        "<br> Message:" + document.getElementById("message").value
-    }).then(
-        message => alert("message sent successfully")
-    );
+// function sendEmail() {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "charansupriya@gmail.com",
+//         Password: "fqbqzobkhycpyfhv",
+//         To: 'reddycharan173@gmail.com',
+//         From: document.getElementById("email").value,
+//         Subject: "New contact form entry",
+//         Body: 
+//         "Name:" + document.getElementById("name").value + 
+//         "<br> Company:" + document.getElementById("company").value + 
+//         "<br> Email:" + document.getElementById("email").value + 
+//         "<br> Phone no:" + document.getElementById("phone").value + 
+//         "<br> Message:" + document.getElementById("message").value
+//     }).then(
+//         message => alert("message sent successfully")
+//     );
 
-}
+// }
 
+document.querySelector('.submitbutton')
+    .addEventListener('submit', () => {
+        window.location.href = 'https://app.netlify.com/sites/biste/overview';
+    });
