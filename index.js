78fa75be0fe8683bac469a3d6c32eb7a08534924
Charan P <charansupriya@gmail.com>
2023-01-19 12:09:31 +0530

    scroll change

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+const text = document.querySelector('.text o');
+text.innerHTML = text.innerText.split("").map(
+    (char, i) =>
+        `<span style= "transform:rotate(${i * 10}deg)">${char}</span>`
+).join("")
