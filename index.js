b08a7fb052a0d44406057672e5777c0e1b10acb1
Charan P <charansupriya@gmail.com>
2023-01-23 00:04:35 +0530

    menu, padland

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ text.innerHTML = text.innerText.split("").map(
     (char, i) =>
         `<span style= "transform:rotate(${i * 6}deg)">${char}</span>`
 ).join("")
+
+const btn = document.getElementById('bar1');
+btn.addEventListener('click', function handleClick() {
+  const initialText = 'menu';
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.textContent = 'close';
+  } else {
+    btn.textContent = initialText;
+  }
+});
