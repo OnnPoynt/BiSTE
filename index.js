de287532c7fcdc1278344a2c666c1beb372f565b
Charan P <charansupriya@gmail.com>
2023-01-30 16:09:38 +0530

    overhaul

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+  menu.classList.toggle('open');
 }
-
-const text = document.querySelector('.text o');
-text.innerHTML = text.innerText.split("").map(
-    (char, i) =>
-        `<span style= "transform:rotate(${i * 6}deg)">${char}</span>`
-).join("")
 
 const btn = document.getElementById('bar1');
 btn.addEventListener('click', function handleClick() {
-  const initialText = 'menu';
+const initialText = 'menu';
 
-  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
-    btn.textContent = 'close';
-  } else {
-    btn.textContent = initialText;
-  }
+if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+  btn.textContent = 'close';
+} else {
+  btn.textContent = initialText;
+}
 });
